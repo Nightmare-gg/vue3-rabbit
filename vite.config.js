67baa4b099d817d9d33a009c008aed61e6085c62
kolt <2787725910@qq.com>
2023-06-02 17:08:49 +0800

    ElementPlus自动按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,25 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// Element plus按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 注册两个插件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
+    // 实际完成路径变更
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
