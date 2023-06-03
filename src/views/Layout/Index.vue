6519c01998f5506604621e97402db38dc264aadf
kolt <2787725910@qq.com>
2023-06-03 11:01:45 +0800

    吸顶导航的交互效果

--- src/views/Layout/Index.vue
+++ src/views/Layout/Index.vue
@@ -2,12 +2,13 @@
 import LayoutNav from './components/LayoutNav.vue';
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
-
+import LayoutFixed from './components/LayoutFixed.vue';
 </script>
 
 <template>
     <Layout-nav/>
     <Layout-header/>
+    <LayoutFixed/>
     <!-- 二级路由出口 -->
     <RouterView/>
     <Layout-footer/>
