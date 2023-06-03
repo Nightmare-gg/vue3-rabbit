a6fff464c92c5fc97d00af07025011ec8bbcd090
kolt <2787725910@qq.com>
2023-06-03 16:51:31 +0800

    home-懒加载指令优化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import {lazyPlugin} from '@/directives/Index'
 
 import App from './App.vue'
 import router from './router'
@@ -15,7 +16,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
 
 
