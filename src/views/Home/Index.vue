92c5f2242c33fec73ea50c826a61732cf7b9a18d
kolt <2787725910@qq.com>
2023-06-03 15:54:34 +0800

    home-面板插件封装

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,11 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  <HomePanel title="新鲜好物" sub-title="新鲜好物副标题">
+    <div>新鲜好物内容</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐副标题">
+    <div slot="main">新鲜好物内容</div>
+  </HomePanel>
 </template>
