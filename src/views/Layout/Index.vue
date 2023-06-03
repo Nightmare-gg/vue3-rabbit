a2582a889cdf00ca1d7c597066f586d689f1bfe6
kolt <2787725910@qq.com>
2023-06-03 10:06:20 +0800

    layout-静态结构搭建

--- src/views/Layout/Index.vue
+++ src/views/Layout/Index.vue
@@ -1,5 +1,14 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue';
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+
+</script>
+
 <template>
-    <h2>我是首页</h2>
+    <Layout-nav/>
+    <Layout-header/>
     <!-- 二级路由出口 -->
     <RouterView/>
+    <Layout-footer/>
 </template>
