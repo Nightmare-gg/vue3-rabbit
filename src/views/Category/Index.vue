c33c4f99d3bb53e4ac74313392c4acea650f042a
kolt <2787725910@qq.com>
2023-06-06 18:07:45 +0800

    激活状态控制和分类列表渲染

--- src/views/Category/Index.vue
+++ src/views/Category/Index.vue
@@ -3,12 +3,14 @@ import {getCategoryAPI} from '@/apis/category'
 import { ref,onMounted } from 'vue';
 import {useRoute} from 'vue-router'
 import { getBannerAPI } from '@/apis/home';
+import GoodsItem from '../Home/components/GoodsItem.vue';
 // 获取数据
 const categoryDate=ref({})
 const route = useRoute()
 const getCategory = async ()=> {
     const res = await getCategoryAPI(route.params.id)
     categoryDate.value = res.result
+    
 }
 onMounted(()=>getCategory())
 
@@ -43,6 +45,25 @@ onMounted(()=> getBanner())
             <img :src="item.imgUrl" alt="">
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryDate.children" :key="i.id">
+            <RouterLink :to="`/category/sub/${i.id}`">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryDate.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
       </div>
     </div>
   </div>
--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -17,7 +17,7 @@ const categoryStore = useCategoryStore()
         </li>
 
         <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-          <RouterLink :to="`/category/${item.id}`">{{item.name}}</RouterLink>
+          <RouterLink active-class="active" :to="`/category/${item.id}`">{{item.name}}</RouterLink>
         </li>
            </ul>
       <div class="search">
