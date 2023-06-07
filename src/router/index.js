fd86a4b9278e01450ede7133180639e3382df00c
kolt <2787725910@qq.com>
2023-06-07 09:39:15 +0800

    整体认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Login from '@/views/Login/Index.vue'
 import Layout from '@/views/Layout/Index.vue'
 import Home from '@/views/Home/Index.vue'
 import Category from '@/views/Category/Index.vue'
+import SubCategory from '@/views/SubCategory/Index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
         {
           path: 'category/:id',
           component: Category
+        },
+        {
+          path: 'category/sub/:id',
+          name: 'subCategory',
+          component: SubCategory
         },
       ]
    },
