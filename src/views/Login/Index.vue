faae7f495f8cf96fe0c044d2b7d1ee1e7a40bebb
kolt <2787725910@qq.com>
2023-06-07 16:46:41 +0800

    登录-统一验证

--- src/views/Login/Index.vue
+++ src/views/Login/Index.vue
@@ -18,7 +18,7 @@ const rules = {
     {required: true, message: '密码不能为空', trigger: 'blur'},
     {min: 6, max: 14,message: '密码长度为6-14个字符',trigger: 'blur'}
   ],
-  argee: [
+  agree: [
     {
       validator: (rule,value,callback)=> {
         // 自定义校验逻辑
@@ -31,6 +31,19 @@ const rules = {
       }
     }
   ]
+}
+// 3.获取form 实例做统一校验
+const formRef = ref(null)
+const doLogin =()=> {
+  // 调用实例方法
+  formRef.value.validate((valid)=> {
+    // valid:所有表单都通过校验 才为true
+    console.log(valid);
+    // 以valid作为判断条件,如果通过校验才执行登录逻辑
+    if(valid) {
+
+    }
+  })
 }
 </script>
 
@@ -56,7 +69,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form :model="form" :rules="rules" ref="formRef" label-position="right" label-width="60px"
               status-icon >
               <el-form-item prop="account"  label="账户">
                 <el-input v-model="form.account"/>
@@ -69,7 +82,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
