b8aafa7f38ca275fffabf31c5c4ec8d44c799882
kolt <2787725910@qq.com>
2023-06-07 15:00:21 +0800

    图片预览组件-放大镜的实现

--- src/components/ImageView/Index.vue
+++ src/components/ImageView/Index.vue
@@ -23,8 +23,14 @@ const {elementX,elementY,isOutside} = useMouseInElement(target)
 // 3.控制花开跟随鼠标移动(监听elementX/Y变化,一旦变化 重新设置left/top)
 const left = ref(0)
 const top = ref(0)
-watch([elementX,elementY],()=> {
+
+// 大图位置
+const positionX = ref(0)
+const positionY = ref(0)
+watch([elementX,elementY,isOutside],()=> {
     // console.log('xy变化了');
+    // 如果鼠标没有移入到盒子里面 直接不执行后面的逻辑
+    if(isOutside.value) return
     // 有效范围内控制滑块距离
     // 横向
     if(elementX.value > 100 && elementX.value <300) {
@@ -41,6 +47,9 @@ watch([elementX,elementY],()=> {
 
     if(elementY.value > 300) {top.value= 200}
     if(elementY.value < 100) {top.value = 0}
+
+    positionX.value = -left.value * 2
+    positionY.value = -top.value * 2
 })
 </script>
 
@@ -51,7 +60,7 @@ watch([elementX,elementY],()=> {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -63,10 +72,10 @@ watch([elementX,elementY],()=> {
     <div class="large" :style="[
       {
         backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
       },
-    ]" v-show="false"></div>
+    ]" v-show="!isOutside"></div>
   </div>
 </template>
 
