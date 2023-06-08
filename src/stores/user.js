52722505374ea66af5aca7fcaddd3d857010f82d
kolt <2787725910@qq.com>
2023-06-08 16:24:35 +0800

    合并购物车

--- src/stores/user.js
+++ src/stores/user.js
@@ -4,6 +4,7 @@ import { defineStore } from "pinia";
 import {ref} from 'vue'
 import {loginAPI} from '@/apis/User'
 import {useCartStore} from'./cartStore'
+import {mergeCartAPI} from '@/apis/cart'
 
 export const useUserStore = defineStore('user',()=> {
    const cartStore= useCartStore()
@@ -14,6 +15,15 @@ export const useUserStore = defineStore('user',()=> {
     const getUserInfo = async ({account,password})=> {
         const res = await loginAPI({account,password})
         userInfo.value = res.result
+        // 合并购物车的操作
+        await mergeCartAPI(cartStore.cartList.map(item=> {
+            return {
+                skuId: item.skuId,
+                selected: item.selected,
+                count: item.count
+            }
+        }))
+        cartStore.updateNewList()
     }
 // 退出时清空用户信息
     const clearUserInfo =()=> {
