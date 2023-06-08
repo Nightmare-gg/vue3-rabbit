1bd0a682d05c7bcb9f1a2d8ebd1333fd643fc67f
kolt <2787725910@qq.com>
2023-06-08 09:36:42 +0800

    token失效401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -23,11 +24,21 @@ const httpInstance = axios.create({
   
   // axios响应式拦截器
   httpInstance.interceptors.response.use(res => res.data, e => {
-  //  统一错误提示
+    const userStore = useUserStore()
+    //  统一错误提示
   ElMessage({
     type: 'warning',
     message: e.response.data.message
   })
+// 401token失效处理
+  if(e.response.status === 401) {
+// 1.清除本地用户数据
+    userStore.clearUserInfo()
+    // 2.跳转登录页
+    router.push('/login')
+  }
+
+
     return Promise.reject(e)
   })
   
