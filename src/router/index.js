63deec32a46def4dcf05720aa6479cd27ca7f04a
kolt <2787725910@qq.com>
2023-06-09 10:18:15 +0800

    会员中心-整体功能梳理和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,10 @@ import CartList from '@/views/CartList/Index.vue'
 import Checkout from '@/views/Checkout/Index.vue'
 import Pay from '@/views/Pay/Index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/Index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -52,6 +56,20 @@ const router = createRouter({
          {
           path:'paycallback',
           component: PayBack
+         },
+         {
+          path: 'member',
+          component: Member,
+          children: [
+            {
+              path: 'user',
+              component: UserInfo
+            },
+            {
+              path: 'order',
+              component: UserOrder
+            }
+          ]
          }
       ]
    },
