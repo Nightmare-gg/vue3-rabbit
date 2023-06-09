12039300fc1cd2d5e67839c68433b721d7dc5876
kolt <2787725910@qq.com>
2023-06-09 10:05:35 +0800

    封装倒计时函数

--- src/views/Pay/Index.vue
+++ src/views/Pay/Index.vue
@@ -2,14 +2,17 @@
 import {getOrderAPI} from '@/apis/pay'
 import {onMounted, ref} from 'vue'
 import { useRoute } from 'vue-router';
+import {useCountDown} from '@/composables/useCountDown'
 const payInfo = ref({})
-
+const {start, formatTime} = useCountDown()
 const route = useRoute()
 
 // 获取订单数据
 const getPayInfo = async ()=> {
     const res = await getOrderAPI(route.query.id)
     payInfo.value = res.result
+     // 初始化倒计时函数
+     start(res.result.countdown)
 }
 
 onMounted(()=> getPayInfo())
@@ -32,7 +35,7 @@ const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redire
         <span class="icon iconfont icon-queren2"></span>
         <div class="tip">
           <p>订单提交成功！请尽快完成支付。</p>
-          <p>支付还剩 <span>24分30秒</span>, 超时后将取消订单</p>
+          <p>支付还剩 <span>{{ formatTime}}</span>, 超时后将取消订单</p>
         </div>
         <div class="amount">
           <span>应付总额：</span>
--- src/views/Pay/PayBack.vue
+++ src/views/Pay/PayBack.vue
@@ -3,14 +3,18 @@ import { getOrderAPI } from '@/apis/pay';
 import {onMounted, ref} from 'vue'
 import { useRoute } from 'vue-router';
 
+
 const route=useRoute()
 const orderInfo = ref({})
 const getOrderInfo = async ()=> {
     const res = await getOrderAPI(route.query.orderId)
     orderInfo.value = res.result
+   
 }
 
 onMounted(()=> getOrderInfo())
+
+
 </script>
 
 
