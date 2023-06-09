d8050f41acd98804a4f76c8eaaf45428f2d26014
kolt <2787725910@qq.com>
2023-06-09 10:48:22 +0800

    会员中心-订单列表-基础订单列表的实现

---
+++ src/apis/order.js
@@ -0,0 +1,18 @@
+// 封装订单接口
+import request from '@/utils/http'
+/*
+params: {
+	orderState:0,
+  page:1,
+  pageSize:2
+}
+*/
+
+
+export const getUserOrder = (params) => {
+    return request({
+      url:'/member/order',
+      method:'GET',
+      params
+    })
+  }
